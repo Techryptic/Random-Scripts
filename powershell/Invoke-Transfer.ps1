3087b0d152b7a90245e1e6be2a75337c7bf2f11c
Techryptic <23514939+Techryptic@users.noreply.github.com>
2020-04-19 02:57:59 +0000

    Fixed Chicken and egg issue.
    When you run Invoke-Command with the FilePath paramater, it executes the server inwhich you put in. On the server it will have the compiled executable which is great and all, but what if you need to invoke-command or ps-session into another server from there?
    I welcome you to the chicken and the egg, I added a bit where it will compile the powershell file (the one that is used on the filepath parameter), it would base64 that file, and then add the base64 code to itself by appending it to the same file. Now, when executed with Invoke-Command, it will write two files to the second (or third, and so on) servers: The compiled executable, and the powershell script that makes it.

--- powershell/Invoke-Transfer.ps1
+++ powershell/Invoke-Transfer.ps1
@@ -1,5 +1,4 @@
 Function Invoke-Transfer {
-
 <#
 .SYNOPSIS
 Invoke-Transfer will allow you to transfer files using a powershell session.
@@ -9,14 +8,6 @@ This powershell script will take a input file, base64 it and create
 another powershell script inwhich you can now use the Invoke-Command's 
 FilePath paramter to load the script directly ontop another powershell session.
 
-The creation of this started when I ran into the famous double-hop powershell
-session issue. On my vm machine Tech.domain.local, I had a ps-session with srv.domain.local.
-srv.domain.local. Within srv.domain.local, it was used to pass information to a database in
-another domain called internal.local. I had credentials to use to access srv2.internal.local,
-which I would Invoke-Command's ScriptBlock to get code-execuation on. I couldn't transfer 
-files the traditional way as it was a one way trust boundary, couldn't use another ps-session
-due to double-hop issues (& credssp was enabled on both!). Invoke-Transfer allows me to 
-transfer my exfiltration executables/scripts onto srv2.internal.local with ease.
 
 .PARAMETER File
 Name of File that is to be transfer.
@@ -25,9 +16,9 @@ Name of File that is to be transfer.
 PS > Invoke-Transfer -File SharpHound.exe
         This will create a file in the same directory called Mod_SharpHound.exe.ps1
 PS > Invoke-Command -ComputerName <Server> -FilePath .\Mod_SharpHound.exe.ps1
-        After ran, this will now create a file in the $home directory of the server machine,
+        This will now create a file in the $home directory of the server machine,
         in this case, Mod_SharpHound.exe is located on <server>.
-PS > Invoke-Command -ComputerName <Server> -ScriptBlock{.\Mod_SharpHound.exe}
+PS > Invoke-Command -ComputerName <Server> -ScriptBlock{.\Mod_SharpHound.exe.ps1}
         This will run the application on the server.
 
 .LINK
@@ -37,29 +28,43 @@ https://techryptic.github.io
 
 [cmdletbinding()]
 Param (
-[string]$File
+[string]$file
 )
 
- Process {
+Process {
 "Checking if $file exist..."
 
 if (Test-Path $file -PathType leaf)
 {
-$fileName = $file
-$fileContent = get-content $fileName -raw
+$fileContent = get-content $file -raw
 $fileContentBytes = [System.Text.Encoding]::UTF8.GetBytes($fileContent)
 $fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
 $finalfile = "Mod_" + $file
+$justname = [io.path]::GetFileNameWithoutExtension("$file")
 
 $Title = '$base64data = "'
-$Output = “This is TEST”
 $Title + 
 $fileContentEncoded + 
-'"' + "`n`n" + 
+'"' + "`n" + 
 "`$decodedata = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(`$base64data))" + 
 "`n"+
-"Add-Content `$home\Documents\$finalfile `$decodedata" | Out-file “$finalfile.ps1”
+"Add-Content `$home\Documents\$finalfile `$decodedata"+ "`n`n" | Out-file “$finalfile.ps1”
 
+#base64'ing itself. (Chicken and the egg)
+$fileContent2 = get-content “$finalfile.ps1” -raw
+$fileContentBytes2 = [System.Text.Encoding]::UTF8.GetBytes($fileContent2)
+$fileContentEncoded2 = [System.Convert]::ToBase64String($fileContentBytes2)
+$decodedata = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($fileContentEncoded2))
+##############################
+#Writing itself, to itself encoded
+$Title = '$base64dataps1 = "'
+$Title + 
+$fileContentEncoded2 + 
+'"' + "`n" + 
+"`$decodedata2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(`$base64dataps1))" + 
+"`n"+
+"Add-Content `$home\Documents\$finalfile-2.ps1 `$decodedata2" | Out-file “$finalfile.ps1” -Append
+##############################
 "Finished!"
 ""
 }
@@ -68,6 +73,5 @@ else {
 ""
 "DOES NOT EXIST"
 }}}
-
 # Uncomment the command below to run script automatically with it's own parameters.
-#Invoke-Transfer -File SharpView.exe
+Invoke-Transfer -File SharpHound.exe
